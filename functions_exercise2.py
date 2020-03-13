87bd729ef956370f2d3e980682da86979bc23ea4
iskskskska <amebina@up.edu.ph>
2020-03-13 20:19:27 +0800

    changed arangement of code

--- functions_exercise2.py
+++ functions_exercise2.py
@@ -1,23 +1,17 @@
 print ("Translating your scores to grades...")
 def grade():
-    if score <0.0:
+    if score <0.0 or score>1.0:
         print ("bad score")
-    elif score == 0.0:
-        print ("Grade = F")
+    elif score >= 0.9:
+        print("Grade = A")
+    elif score >= 0.8:
+        print("Grade = B")
+    elif score >= 0.7:
+        print("Grade = C")
+    elif score >= 0.6:
+        print ("Grade = D")
     elif score < 0.6:
         print ("Grade = F")
-    elif score < 0.7:
-        print ("Grade = F")
-    elif score < 0.8:
-        print ("Grade = D")
-    elif score < 0.8:
-        print ("Grade = C")
-    elif score < 0.9:
-        print ('Grade = B')
-    elif score == 1.0:
-        print ('Grade = A')
-    elif score > 1.0:
-        print ("bad score")
 try:
     score= float(input("Enter score:"))
     grade()
