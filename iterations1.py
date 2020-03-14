3768d763fb65d2cf47fe2d7ae13d289dc52db903
iskskskska <amebina@up.edu.ph>
2020-03-14 16:08:26 +0800

    added files

--- iterations1.py
+++ iterations1.py
@@ -1,3 +1,18 @@
-
 x = int(input("Enter a number:"))
-while n>0:
+list=[]
+while True:
+    x=input("Enter number:")
+    try:
+        if x=='done':
+            print("sum=",sum(list))
+            count=0
+            for thing in list:
+                count=count+1
+            print("count=",count)
+            print("average=",(sum(list)/count))
+            break
+        y=int(x)
+        list.append(y)
+    except:
+        print('invalid input')
+print('yehey!')
